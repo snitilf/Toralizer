0b919ef21f4902ef72aa0ffc777802c9a20aeb71
Filip Snitil <filip.snitil@mail.mcgill.ca>
2025-02-27 21:45:21 -0500

    Python project structure established

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="toralizer",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "stem",
+        "requests",
+        "pysocks",
+    ],
+    entry_points={
+        'console_scripts': [
+            'toralize=toralizer.cli:main',
+        ],
+    },
+    author="Your Name",
+    description="A command line tool to redirect network traffic through Tor",
+)
