e83210f4d9c0b5b48e01d471ff37e88e3076911d
Filip Snitil <filip.snitil@mail.mcgill.ca>
2025-11-15 17:50:24 -0500

    restarting

--- setup_dev.sh
+++
@@ -1,8 +0,0 @@
-# Create virtual environment if it doesn't exist
-python3 -m venv venv
-# Activate it
-source venv/bin/activate
-# Install dependencies
-pip install stem requests pysocks
-# Install your package in development mode
-pip install -e .
